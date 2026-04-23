242fd03c48347abe3ec76403a83a78562ae82a9e
Radiks Alijevs <dedalik@gmail.com>
2026-04-23 12:19:55 +0300

    docs(theme): add global import optimization guide across docs

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,8 @@
 import DefaultTheme from "vitepress/theme";
+import { h } from "vue";
 // import Test from "./components/Note.vue";
 import PackageData from "./components/PackageData.vue";
+import ImportGuide from "./components/ImportGuide.vue";
 
 import "./styles/tailwind.css";
 import "./styles/styles.css";
@@ -11,6 +13,11 @@ import "./styles/overrides.css";
 
 export default {
   ...DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      "doc-before": () => h(ImportGuide),
+    });
+  },
   enhanceApp({ app }) {
     // register your custom global components
     app.component("PackageData", PackageData);
